a7fe68e00a07c4ade9e59364e1b47f8d608970b1
Sine Uyeno <Jude.Uyeno@edu.sait.ca>
2025-09-23 14:00:15 -0600

    I apologize for my casualness

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,10 +3,8 @@ import { NewItem } from "./new-item";
 export default function Page() {
   return (
     <main>
-        <h1 class="text-4xl font-sans">THE COUNTER THING!!!</h1>
+        <h1 class="text-4xl font-sans">Inc/Dec Counter</h1>
         <NewItem/>
-        <h1 class="text-4xl font-sans">THE COUNTER THING!!!</h1>
-        <p>THE COUNTER IS COUNTERING!!!!!</p>
     </main>
   );
 }
