97455c0f271cd321582be906a4a26219254547b7
Sine Uyeno <Jude.Uyeno@edu.sait.ca>
2025-11-11 21:21:35 -0700

    Struggling with the last bit

--- app/week-8/page.js
+++ app/week-8/page.js
@@ -3,7 +3,7 @@ import ItemList from "./item-list";
 import { useState } from "react";
 import { NewItem, userItem } from "./new-item";
 import itemsData from "./items.json";
-import { mealIdeas } from "../week-8/meal-ideas";
+import { MealIdeas } from "./meal-ideas";
 
 
 export default function Page() {
@@ -11,17 +11,30 @@ export default function Page() {
   const [items, setItems] = useState(
     itemsData.map((obj) => obj)
   );
+
+  const [selectedItemName, setSelectedItemName] = useState("");
   
   const handleAddItem = () => {
     items.push(userItem);
   };
 
+  const handleItemSelect = () => {
+    console.log("aaaa");
+  }
+
   return (
     <main>
       <h1 className="text-4xl font-sans italic">Shopping List</h1>
-      <NewItem onAddItem={handleAddItem}/>
-      <ItemList items={items}/>
-      <mealIdeas/>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div>
+          <NewItem onAddItem={handleAddItem} onItemSelect={handleItemSelect}/>
+          <ItemList items={items}/>
+        </div>
+        <MealIdeas ingredient="egg"/>
+        
+      </div>
+      
+      
     </main>
   );
 }
